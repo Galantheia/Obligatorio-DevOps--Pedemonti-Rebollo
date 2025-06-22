a60918284c30798aa85e05a9084746c7b3f6bfe3
Alvaro Rebollo <alvaro.rebollo91@gmail.com>
2025-06-22 17:21:13 -0300

    Validacion inicial de argumentos

--- backup_setuid.sh
+++ backup_setuid.sh
@@ -1,1 +1,19 @@
-#!/bin/bash
+#!bin/bash
+
+#Compruebo que se haya ingresado un solo argumento
+
+if [ $# != 1 ]; then
+        echo "Debe ingresar un solo parametro"
+        echo "USO: ./backup_setuid.sh <directorio>"
+        exit 1
+fi
+
+
+#Compruebo que se haya dado como argumento un directorio
+
+if [ ! -d $1 ]; then
+        echo "El parametro ingresado no es un directorio"
+        echo "USO: ./backup_setuid.sh <directorio>"
+        exit 2
+fi
+
