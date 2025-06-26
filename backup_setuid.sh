6c7998709283e53925443bb73e1ed25c9261f986
Alvaro Rebollo <alvaro.rebollo91@gmail.com>
2025-06-26 01:38:51 -0300

    Mejora de busqueda y creacion de archivos log y tar.gz

--- backup_setuid.sh
+++ backup_setuid.sh
@@ -24,12 +24,35 @@ echo "Directorio: $DIRECTORIO"
 echo "Crear_Log: $Crear_Log"
 echo "Bash: $Bash"
 
+#Creacion de archivos de log y tar
+Fecha=$(date +"%m-%d-%y_%H-%M-%S")
+Logs="Logs_Caminos_${Fecha}.rep"
+Tar="Backups_${Fecha}.tar.gz"
+
+
+
 #Buscar ejecutables
 
 
-find "$DIRECTORIO" -type f -perm -4000 -perm -0001 | while read -r archivo; do
-        echo "Analizando archivo: $archivo"
-        if head -n 1 "$archivo" | grep -qE '^#!'; then
-               echo "script encontrado: $archivo"
+#find "$DIRECTORIO" -type f -perm -4000 -perm -0001 | while read -r archivo; do
+#        echo "Analizando archivo: $archivo"
+#        if head -n 1 "$archivo" | grep -qE '^#!'; then
+#               echo "script encontrado: $archivo"
+#        fi
+#done
+
+
+while IFS= read -r archivo; do
+        if $Bash; then
+                head -n 1 "$archivo" | grep -q '^#!/bin/bash'
         fi
+        echo "Script bash encontrado: $archivo"
+        if $Crear_Log; then
+                echo "$archivo" >> "$Logs"
+        fi
+
+        Scripts_encontrados+=("$archivo")
+
+done < <(find "$DIRECTORIO" -type f -perm -4000 -perm -0001)
+
 
